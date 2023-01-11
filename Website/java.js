79ccfdbf861eeb9f923938bec354aa7f97fb9714
Majoros-Peter <70958388+Majoros-Peter@users.noreply.github.com>
2023-01-11 23:05:56 +0100

    Add files via upload

--- Website/java.js
+++ Website/java.js
@@ -67,18 +67,38 @@ once: true,
 
 
 function selectLan() {
-let select = document.getElementById('language').value ;
-let rules1 = document.getElementById('Rules1');
-let rules2 = document.getElementById('Rules2');
-let rules3 = document.getElementById('Rules3');
- if(document.getElementById('language').value === 'eu'){
- document.getElementById('rulesTitle').innerHTML = "ANGOL";
- rules1.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa laudantium blanditiis fugit"
+ if(document.getElementById('language').value === 'eu')
+ {
+    document.getElementById("Rules1").innerHTML = "Navigating in Menu:";
+    document.getElementById("made").innerHTML = "Made By:";
+    document.getElementById("project").innerHTML = "December Project Work";
+    document.getElementById("labirint").innerHTML = "Labirynth";
+    document.getElementById('nav2').innerHTML = "RULES";
+    document.getElementById('nav3').innerHTML = "THE GAME";
+    document.getElementById('nav4').innerHTML = "THE TEAM";
+    document.getElementById('rulesTitle').innerHTML = "RULES";
+    document.getElementById("RulesText1").innerHTML = "You can navigate through the menu by pressing ↑ ↓ ENTER and go back one step (or exit) by pressing ESC.";
+    document.getElementById("RulesText2").innerHTML = "In the map editor you can move around by default using the ↑ ↓ → ← buttons. Items can be selected using the characters shown below them. You can rotate the selected map elements by pressing the + - buttons and unload them by pressing ENTER. When the track is done, you just press ESC and if it is done correctly, you can save it.";
+    document.getElementById("RulesText3").innerHTML = "In the game, control is by default with the ↑ ↓ → ← buttons. The goal of the game is to get to (and out of) as many treasure rooms as possible. The difficulty of the game can be increased by using a covered map, a light maze, blindness";
+    document.getElementById("SlidesText1").innerHTML = "EDITING THE LABYRINTH: After specifying the width and height of the labyrinth (20;10 in the picture), you can move with the arrows (you must place at least 1 treasure room to make it regular).";
+    document.getElementById("SlidesText2").innerHTML = "TO CREATE A LABYRINTH: After entering the width and height of the maze (30;15 in the picture), you have to enter the starting coordinate, the centre of the track is recommended (the program will print this). After that you can further modify the maze (you have to add at least 1 treasure room to make it regular).";
+    document.getElementById("SlidesText3").innerHTML = "MODIFYING AN EXISTING LABYRINTH: it starts by specifying the name of an existing labyrinth (no path, no extension, just the name of the labyrinth).";
+    
+  }
+  else
+  {
+    document.getElementById("made").innerHTML = "Készítette:";
+    document.getElementById("project").innerHTML = "Decemberi Projektmunka";
+    document.getElementById("labirint").innerHTML = "Labirintus";
+    document.getElementById('nav2').innerHTML = "Szabályzatról";
+    document.getElementById('nav3').innerHTML = "Játékról";
+    document.getElementById('nav4').innerHTML = "Csapatról";
+    document.getElementById('rulesTitle').innerHTML = "SZABÁLYZAT";
+    document.getElementById("RulesText1").innerHTML = "Menüben navigálás";
+    document.getElementById("RulesText2").innerHTML = "Térkép szerkesztőben irányítás";
+    document.getElementById("RulesText3").innerHTML = "Játékban irányítás";
+    document.getElementById("SlidesText1").innerHTML = "SAJÁT LABIRINTUS SZERKESZTÉSE: A labirintus szélessége és magassága (a képen 20;10) megadása után a nyilakkal lehet mozogni (legalább 1 kincses termet muszáj rakni, hogy szabályos legyen).";
+    document.getElementById("SlidesText2").innerHTML = "LABIRINTUS GENERÁLÁSA: A labirintus szélessége és magassága (a képen 30;15) megadása után meg kell még adni a kezdőkoordinátát, a pálya közepe az ajánlott (ezt majd kiírja a program). Ezután lehet tovább módosítani még a labirintuson (legalább 1 kincses termet muszáj rakni, hogy szabályos legyen).";
+    document.getElementById("SlidesText3").innerHTML = "MEGLÉVŐ LABIRINTUSON MÓDOSÍTÁS: Azzal kezdődik, hogy meg kell egy létező labirintus nevét (nem kell elérési út, se kiterjesztés, csak a labirintus neve).";
+  }
 }
-else{
-  document.getElementById('rulesTitle').innerHTML = "SZABÁLYZAT/IRÁNYÍTÁS";
-  rules1.innerHTML = "Menüben navigálás"
-  rules2.innerHTML = "Térkép szerkesztőben irányítás"
-  rules3.innerHTML = "Játékban irányítás"
-}
- }
